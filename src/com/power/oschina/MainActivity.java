a4475ac56b8c81b1c2ddef51e1345dfc3607f185
jinjianxin <jinjianxinjin@gmail.com>
2014-11-12 15:41:00 +0800

    增加页面滚动部分

--- src/com/power/oschina/MainActivity.java
+++ src/com/power/oschina/MainActivity.java
@@ -5,37 +5,60 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.RadioButton;
 
 import com.loopj.android.http.*;
 
 
 public class MainActivity extends Activity {
+	
+	private RadioButton m_news = null;
+	private RadioButton m_question = null;
+	private RadioButton m_tweet = null;
+	private RadioButton m_active = null;
+	private RadioButton m_more = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        
+        initRadioButton();
+        
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    
+    private void initRadioButton()
+    {
+    	m_news = (RadioButton) findViewById(R.id.foot_news);
+    	m_question = (RadioButton) findViewById(R.id.foot_question);
+    	m_tweet = (RadioButton) findViewById(R.id.foot_tweet);
+    	m_active =(RadioButton) findViewById(R.id.foot_active);
+    	m_more = (RadioButton) findViewById(R.id.foot_more);
+    	
+    	m_news.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+			m_question.setChecked(false);
+			m_tweet.setChecked(false);
+			m_active.setChecked(false);
+			m_more.setChecked(false);
+				
+			}
+		});
+    	
+    	m_question.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
     }
 }
